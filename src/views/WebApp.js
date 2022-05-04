5fda39ad4200172cd7094d3a7d3acbc48435d055
Adrian Gri <adrian.gri03@gmail.com>
2022-05-04 18:56:58 -0400

    added webapp boilerplate screen

---
+++ src/views/WebApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+import FeaturesSplit from '../components/sections/FeaturesSplit';
+import Testimonial from '../components/sections/Testimonial';
+import Cta from '../components/sections/Cta';
+import WebAppComponent from '../components/sections/WebAppComponent';
+
+const WebApp = () => {
+
+  return (
+    <>
+      <WebAppComponent className="illustration-section-01" />
+      {/* <Cta split /> */}
+    </>
+  );
+}
+
+export default WebApp;
