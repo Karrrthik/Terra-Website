ed2350256ab7cfeedde5326ddc1c263c65fd590d
Karthik Nambiar <k3nambia@uwaterloo.ca>
2022-05-06 18:50:49 -0400

    added build tool

---
+++ build/precache-manifest.8115f2ac21fcbfb543b76fe053a3ba5f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8852f791926537d1c2358692105e4765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9f8e3530766d983618",
+    "url": "/static/css/main.b4d60e9f.chunk.css"
+  },
+  {
+    "revision": "b5cd6a8bfab18ff21341",
+    "url": "/static/js/2.9a85aafe.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.9a85aafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9f8e3530766d983618",
+    "url": "/static/js/main.ad33d88c.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "b92340ea9aed5ffcc1947207cf193d26",
+    "url": "/static/media/adrian.b92340ea.png"
+  },
+  {
+    "revision": "01884e011803765faf6bdd092e3abb3a",
+    "url": "/static/media/chris.01884e01.jpg"
+  },
+  {
+    "revision": "307bc9ddb00ec1cf406d512132102892",
+    "url": "/static/media/codeblock.307bc9dd.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "1072a8c035aa673675dd8addc17a430e",
+    "url": "/static/media/karthik.1072a8c0.png"
+  },
+  {
+    "revision": "326ff47dbe0da2f3f88c5db7de665ee1",
+    "url": "/static/media/logo.326ff47d.svg"
+  }
+]);
